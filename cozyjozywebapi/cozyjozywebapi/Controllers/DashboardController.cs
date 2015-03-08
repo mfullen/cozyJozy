e94c78d5b98b2fe793711c2ba51db1a096aa49bd
Mike Fullen <mike@mikefullen.com>
2015-03-08 15:18:51 -0400

    Fixed Amount per Feed in last 24 hours.

--- cozyjozywebapi/cozyjozywebapi/Controllers/DashboardController.cs
+++ cozyjozywebapi/cozyjozywebapi/Controllers/DashboardController.cs
@@ -43,7 +43,7 @@ namespace cozyjozywebapi.Controllers
                 ChildId = childId,
                 NumberOfRecentDiaperChanges = _context.DiaperChanges.Where(c=>c.ChildId == childId).Count(d => d.OccurredOn > recent),
                 NumberOfRecentFeedings = _context.Feedings.Where(c => c.ChildId == childId).Count(d => d.EndTime > recent),
-                RecentAmountPerFeed = recentAmountPerFeed != null ? recentAmountPerFeed.Value : 0,
+                RecentAmountPerFeed = recentAmountPerFeed != null ? recentAmountPerFeed.Value / _context.Feedings.Where(c => c.ChildId == childId).Count(d => d.EndTime > recent) : 0,
                 TotalAmountPerFeed = totalAmountPerFeed != null ? totalAmountPerFeed.Value / _context.Feedings.Count(c => c.ChildId == childId) : 0
             };
 
