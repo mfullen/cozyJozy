d7f57b9766f10ef212577d9fb2866c7e284e7ca6
Mike Fullen <mike@mikefullen.com>
2015-03-14 19:59:22 -0400

    Infrastructure for Unit of work and repository pattern

---
+++ cozyjozywebapi/cozyjozywebapi/Infrastructure/CozyJozyUnitOfWork.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace cozyjozywebapi.Infrastructure
+{
+    public class CozyJozyUnitOfWork : Disposable, IUnitOfWork
+    {
+        private readonly DbContext _context;
+        private readonly IFeedingRepository _feedingRepository;
+
+        public CozyJozyUnitOfWork(DbContext context, IFeedingRepository feedingRepository)
+        {
+            _context = context;
+            _feedingRepository = feedingRepository;
+        }
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public IFeedingRepository FeedingRepository
+        {
+            get { return _feedingRepository; }
+        }
+    }
+
+    public class Disposable : IDisposable
+    {
+        private bool _isDisposed;
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Disposable()
+        {
+            Dispose(false);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_isDisposed && disposing)
+            {
+                DisposeCore();
+            }
+
+            _isDisposed = true;
+        }
+
+        protected virtual void DisposeCore()
+        {
+        }
+    }
+}
---
+++ cozyjozywebapi/cozyjozywebapi/Infrastructure/RepositoryBase.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Web;
+
+namespace cozyjozywebapi.Infrastructure
+{
+    public abstract class RepositoryBase<T> : IRepository<T>
+        where T : class
+    {
+        private readonly IDbSet<T> _dbset;
+        private readonly DbContext _dataContext;
+
+        protected RepositoryBase(DbContext context)
+        {
+            _dataContext = context;
+            _dbset = context.Set<T>();
+        }
+
+        protected DbContext Table
+        {
+            get { return _dataContext; }
+        }
+
+        public virtual T Add(T entity)
+        {
+            return _dbset.Add(entity);
+        }
+
+        public virtual T Update(T entity)
+        {
+            T updatedEntity = _dbset.Attach(entity);
+            _dataContext.Entry(entity).State = EntityState.Modified;
+            return updatedEntity;
+        }
+
+        public virtual T Delete(T entity)
+        {
+            return _dbset.Remove(entity);
+        }
+
+        public virtual IQueryable<T> Delete(Expression<Func<T, bool>> where)
+        {
+            IEnumerable<T> objects = _dbset.Where(where).AsEnumerable();
+            return objects.Select(obj => _dbset.Remove(obj)).AsQueryable();
+        }
+
+        public virtual T GetById(long id)
+        {
+            return _dbset.Find(id);
+        }
+
+        public virtual T GetById(string id)
+        {
+            return _dbset.Find(id);
+        }
+
+        public virtual IQueryable<T> GetAll()
+        {
+            return _dbset;
+        }
+
+        public virtual IQueryable<T> GetMany(Expression<Func<T, bool>> where)
+        {
+            return _dbset.Where(where);
+        }
+
+        public T Get(Expression<Func<T, bool>> where)
+        {
+            return _dbset.Where(where).FirstOrDefault();
+        }
+    }
+}
