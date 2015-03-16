43e499868d16bfe3166b505323db2d5d878a13ea
Mike Fullen <mike@mikefullen.com>
2015-03-16 16:43:55 -0400

    Children controller now properly filters the returning child based on the user logged in

--- cozyjozywebapi/cozyjozywebapi/Controllers/ChildrenController.cs
+++ cozyjozywebapi/cozyjozywebapi/Controllers/ChildrenController.cs
@@ -1,17 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Entity;
+﻿using System.Data.Entity;
 using System.Data.Entity.Infrastructure;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Description;
-using cozyjozywebapi.Entity;
-using cozyjozywebapi.Infrastructure;
 using cozyjozywebapi.Infrastructure.Core;
 using cozyjozywebapi.Models;
 using Microsoft.AspNet.Identity;
@@ -27,18 +21,34 @@ namespace cozyjozywebapi.Controllers
             _unitOfWork = uow;
         }
 
+        public class ChildResponse
+        {
+            public Child Child { get; set; }
+            public bool ReadOnly { get; set; }
+        }
+
+        protected IQueryable<ChildPermissions> FilteredChildren()
+        {
+            var userId = HttpContext.Current.User.Identity.GetUserId();
+            return _unitOfWork.ChildPermissionsRepository.All().Where(c => c.IdentityUserId == userId);
+            //.Select(c => new ChildResponse
+            //{
+            //    Child = c.Child,
+            //    ReadOnly = c.ReadOnly
+            //});
+        }
+
         // GET: api/Children
         public IQueryable<Child> GetChild()
         {
-            var userId = HttpContext.Current.User.Identity.GetUserId();
-            return _unitOfWork.ChildPermissionsRepository.All().Where(c => c.IdentityUserId == userId).Select(c => c.Child);
+            return FilteredChildren().Select(c => c.Child);
         }
 
         // GET: api/Children/5
         [ResponseType(typeof(Child))]
         public async Task<IHttpActionResult> GetChild(int id)
         {
-            Child child = await _unitOfWork.ChildRepository.FindAsync(c=> c.Id == id);
+            Child child =  await FilteredChildren().Where(c=> c.ChildId == id).Select(c=> c.Child).FirstAsync();
             if (child == null)
             {
                 return NotFound();
@@ -57,6 +67,11 @@ namespace cozyjozywebapi.Controllers
             }
 
             if (id != child.Id)
+            {
+                return BadRequest();
+            }
+
+            if(!FilteredChildren().Any(c => c.ChildId == id))
             {
                 return BadRequest();
             }
@@ -97,7 +112,7 @@ namespace cozyjozywebapi.Controllers
             _unitOfWork.ChildPermissionsRepository.Add(new ChildPermissions()
             {
                 ChildId = child.Id,
-                Role = _unitOfWork.RoleRepository.All().FirstOrDefault(c => c.Id == "PARENT"),
+                ReadOnly = false,
                 IdentityUserId = userId
             });
             await _unitOfWork.CommitAsync();
@@ -108,6 +123,11 @@ namespace cozyjozywebapi.Controllers
         [ResponseType(typeof(Child))]
         public async Task<IHttpActionResult> DeleteChild(int id)
         {
+            if (!FilteredChildren().Any(c => c.ChildId == id))
+            {
+                return BadRequest();
+            }
+
             Child child = await _unitOfWork.ChildRepository.FindAsync(c=> c.Id == id);
             if (child == null)
             {
